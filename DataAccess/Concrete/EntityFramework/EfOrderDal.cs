c29ff168b2306b271406c690963a211e79c48dfa
huseyni <huseyinozgurkazan@gmail.com>
2022-07-07 16:30:02 +0300

    07.07.2022 16:29

--- DataAccess/Concrete/EntityFramework/EfOrderDal.cs
+++ DataAccess/Concrete/EntityFramework/EfOrderDal.cs
@@ -5,14 +5,12 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    public class EfOrderDal : EfEntityRepositoryBase<Order, NorthwindContext>, IOrderDal
     {
-
     }
 }
--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,4 +1,4 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +9,7 @@ namespace Entities.Concrete
 {
     public class Product : IEntity
     {
-        public int ProductId { get; set; } 
+        public int ProductId { get; set; }
         public int CategoryId { get; set; }
         public string ProductName { get; set; } = String.Empty;
         public short UnitsInStock { get; set; }
