400fab7c3e956da79f400ea2abf06fc5fa7ddb1a
Hüseyin Özgür Kazan <huseyinozgurkazan@outlook.com>
2022-07-17 18:47:19 +0300

    17.07.2022 18:47

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,10 +10,20 @@ static void ProductTest()
 {
     ProductManager productManager = new ProductManager(new EfProductDal());
 
-    foreach (var product in productManager.GetProductDetail())
+    var result = productManager.GetProductDetail();
+
+    if (result.Success)
     {
-        Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+        foreach (var product in result.Data)
+        {
+            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+        }
     }
+    else
+    {
+        Console.WriteLine(result.Message);
+    }
+
 }
 
 static void CategoryTest()
